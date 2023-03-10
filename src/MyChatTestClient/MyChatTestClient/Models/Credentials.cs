828f35e629a36a4ab85a431d9e918185a2bd518b
Sergei Sulimov <s.v.sulimoff@gmail.com>
2023-03-10 16:19:06 +0400

    Refactoring in Client

---
+++ src/MyChatTestClient/MyChatTestClient/Models/Credentials.cs
@@ -0,0 +1,33 @@
+﻿namespace Sulimov.MyChat.Client.Models
+{
+    /// <summary>
+    /// Class for store credentials.
+    /// </summary>
+    public class Credentials
+    {
+        /// <summary>
+        /// User ID.
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+
+        /// <summary>
+        /// User login.
+        /// </summary>
+        public string Login { get; set; } = string.Empty;
+
+        /// <summary>
+        /// User password.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Email.
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Current token.
+        /// </summary>
+        public string Token { get; set; } = string.Empty;
+    }
+}
